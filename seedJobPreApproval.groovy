c58b4330972e02dc1f2d07b2e5d98da26e4afa0f
Enrique Valenzuela <emvp84@gmail.com>
2019-03-14 13:58:25 -0400

    improve seed job pre approval

--- seedJobPreApproval.groovy
+++ seedJobPreApproval.groovy
@@ -3,9 +3,6 @@ import com.cloudbees.plugins.credentials.domains.*
 import com.cloudbees.plugins.credentials.impl.*
 import com.michelin.cio.hudson.plugins.rolestrategy.*
 import hudson.model.Cause
-import hudson.plugins.s3.S3BucketPublisher
-import hudson.plugins.s3.S3BucketPublisher.DescriptorImpl
-import hudson.plugins.s3.S3Profile
 import hudson.security.*
 import hudson.security.AuthorizationStrategy
 import hudson.security.SecurityRealm
@@ -20,10 +17,6 @@ import org.jenkinsci.plugins.scriptsecurity.scripts.languages.GroovyLanguage;
 
 import jenkins.AgentProtocol
 import hudson.model.RootAction
-
-Jenkins.instance.setNoUsageStatistics(true)
-Jenkins.instance.getSetupWizard()?.completeSetup()
-Jenkins.instance.getDescriptor("jenkins.CLI").get().setEnabled(false)
 
 // Disable deprecated JNLP protocols
 def protocols = AgentProtocol.all()

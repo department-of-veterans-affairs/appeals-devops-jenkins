9f615b756a98bfb78c7bc8fe84ec6b7ae3b7c574
Enrique Valenzuela <emvp84@gmail.com>
2019-12-20 16:13:48 -0500

    more spacing

--- vars/gitCheckout.groovy
+++ vars/gitCheckout.groovy
@@ -2,9 +2,9 @@ def call(Map stageParams) {
 
     checkout([
         $class: 'GitSCM',
-        branches: [[name:  stageParams.branch ]],
+        branches: [[ name:  stageParams.branch ]],
         userRemoteConfigs: [[ url: stageParams.url ]],
         userRemoteConfigs: [[ credentialsId: 'va-bot', url: stageParams.url ]],
-        extensions: [[$class: 'CleanCheckout']]
+        extensions: [[ $class: 'CleanCheckout' ]]
     ])
   }

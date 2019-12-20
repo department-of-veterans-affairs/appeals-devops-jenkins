0659b97b1e69b9da340e7cde712bfa225087ed8e
Enrique Valenzuela <emvp84@gmail.com>
2019-12-20 17:33:49 -0500

    back to basics

--- vars/customSlack.groovy
+++ vars/customSlack.groovy
@@ -3,41 +3,37 @@
 def call(Map stageParams) {
     buildResult = stageParams.buildResult
     
-    slack_message = stageParams.message ? stageParams.message : """**${buildResult}**
-    **Job:** ${env.JOB_NAME}
-    **Build Numer:** ${env.BUILD_NUMBER}
-    ${currentBuild.getAbsoluteUrl()}
-    """.stripMargin()
-
-    url = ${currentBuild.getAbsoluteUrl()}
+    message = stageParams.message ? stageParams.message : """${buildResult}
+    Job: ${env.JOB_NAME}
+    Build Numer: ${env.BUILD_NUMBER}
+    ${currentBuild.getAbsoluteUrl()}""".stripMargin()
 
     success_channel = stageParams.channel
     failure_channel = stageParams.failure_channel ? failure_channel: "appeals-devops"
-    slack_color = "good"
     
     try{
         if ( buildResult == "SUCCESS" ) {
-            slack_color = "good"
-            slack_channel = success_channel
+            slackSend   color: "good",
+                        message: message,
+                        channel: success_channel
 
         }
         else if( buildResult == "FAILURE" ) {
-            slack_color = "danger"
-            slack_channel = failure_channel
+            slackSend   color: "danger",
+                        message: message,
+                        channel: failure_channel
         }
         else if( buildResult == "UNSTABLE" ) {
-            slack_color ="warning"
-            slack_channel = failure_channel
+            slackSend   color: "warning",
+                        message: message,
+                        channel: failure_channel
         }
         else {
-            slack_color = "danger"
-            slack_channel = failure_channel
+            slackSend   color: "danger",
+                        message: message,
+                        channel: failure_channel
 
         }
-
-
-    slackSend(channel: slack_channel , color: slack_color, message: slack_message)
-
     }
     catch(err) {
         println "Failed to notify Slack: ${err}"

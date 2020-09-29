90be21c7af65be21649e2f274d73bdb1ab423f19
Anya Roltsch <aroltsch@tistatech.com>
2020-09-29 08:44:50 -0400

    Add slack token

--- vars/customSlack.groovy
+++ vars/customSlack.groovy
@@ -2,7 +2,7 @@
 
 def call(Map stageParams) {
     buildResult = stageParams.buildResult
-    
+
     message = stageParams.message ? stageParams.message : """${buildResult}
     Job: ${env.JOB_NAME}
     Build Numer: ${env.BUILD_NUMBER}
@@ -10,28 +10,32 @@ def call(Map stageParams) {
 
     success_channel = stageParams.channel
     failure_channel = stageParams.failure_channel ? stageParams.failure_channel : "appeals-devops"
-    
+
     try{
         if ( buildResult == "SUCCESS" ) {
             slackSend   color: "good",
                         message: message,
-                        channel: success_channel
+                        channel: success_channel,
+                        token: env.SLACK_TOKEN
 
         }
         else if( buildResult == "FAILURE" ) {
             slackSend   color: "danger",
                         message: message,
-                        channel: failure_channel
+                        channel: failure_channel,
+                        token: env.SLACK_TOKEN
         }
         else if( buildResult == "UNSTABLE" ) {
             slackSend   color: "warning",
                         message: message,
-                        channel: failure_channel
+                        channel: failure_channel,
+                        token: env.SLACK_TOKEN
         }
         else {
             slackSend   color: "danger",
                         message: message,
-                        channel: failure_channel
+                        channel: failure_channel,
+                        token: env.SLACK_TOKEN
 
         }
     }

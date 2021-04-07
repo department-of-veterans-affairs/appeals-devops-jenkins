80a96115f9798bbab502ad0c1403b4e34f499629
Joel Robinson <jrobinson@tistatech.com>
2021-04-07 15:10:57 -0500

    Fix spelling error.

--- vars/notificationsNewRelic.groovy
+++ vars/notificationsNewRelic.groovy
@@ -16,7 +16,7 @@ def call(Map config) {
     // create a deployment event in New Relic so we can audit our deploy history.
     // for instance, you can see production Caseflow deploys here:
     // https://rpm.newrelic.com/accounts/1788458/applications/80608544/deployments.
-    def new_RELIC_APP_IDS = [
+    def NEW_RELIC_APP_IDS = [
       caseflow: [
         prod: 80608544,
         preprod: 81409034,

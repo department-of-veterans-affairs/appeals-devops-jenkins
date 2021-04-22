65d3c8c6c421a541a175b34c156c9c5c818830a5
skeenb <brian.skeen97@gmail.com>
2021-04-22 14:37:16 -0400

    it changes the attach_asg_to by terragrunt apply now!

--- blue_green/src/main/java/com/example/blue_green/blue_green.groovy
+++ blue_green/src/main/java/com/example/blue_green/blue_green.groovy
@@ -37,14 +37,14 @@
 def String terragrunt_working_dir = '/Users/bskeen/repository/appeals-terraform/live/uat/revproxy-caseflow-replica/common'
 
 public String removeBlankSpace(StringBuilder sb) {
-  	println "Running removeBlankSpace()"
-  	int j = 0;
-  	for(int i = 0; i < sb.length(); i++) {
-  	  if (!Character.isWhitespace(sb.charAt(i))) {
-  	     sb.setCharAt(j++, sb.charAt(i));
-  	  }
-  	}
-  	return sb.delete(j, sb.length());
+  println "Running removeBlankSpace()"
+  int j = 0;
+  for(int i = 0; i < sb.length(); i++) {
+    if (!Character.isWhitespace(sb.charAt(i))) {
+       sb.setCharAt(j++, sb.charAt(i));
+    }
+  }
+  return sb.delete(j, sb.length());
 }
 
 // TODO tomorrow: Test this shit and make sure it works when A=100 and when B=100
@@ -57,9 +57,9 @@ public def get_blue(String terragrunt_working_dir) {
 	proc_a.consumeProcessOutput(sout, serr)
 	proc_a.waitForOrKill(10000)
 	def String blue_weight_a = removeBlankSpace(sout) 
-	if(blue_weight_a.equals('100')) {
+	if (blue_weight_a.equals('100')) {
 		def String blue = 'a'
-		return blue
+	  return blue	
 	}
 	else {
 		def String blue = 'b'
@@ -67,8 +67,42 @@ public def get_blue(String terragrunt_working_dir) {
 	}
 }
 
+public def change_attach_asg_to(blue, terragrunt_working_dir) {
+  println "Running change_attach_asg_to()"
+  if (blue.equals('a')) {
+    attach_asg_to = 'b'    
+  }
+  if (blue.equals('b')) {
+    attach_asg_to = 'a'   
+  }
+  File tfvars = new File("${terragrunt_working_dir}/terraform.tfvars")
+  tfvars.append "attach_asg_to = \"${attach_asg_to}\"\n"
+  tfvars.append "blue_weight_a = 100\n"
+  tfvars.append "blue_weight_b = 0\n"
+  tfvars.append "green_weight_a = 0\n"
+  tfvars.append "green_weight_b = 100\n"
+  def sout = new StringBuilder(), serr = new StringBuilder()
+  "terragrunt init --terragrunt-working-dir ${terragrunt_working_dir}".execute()
+  def proc = "terragrunt apply -auto-approve --terragrunt-working-dir ${terragrunt_working_dir}".execute() 
+  proc.consumeProcessOutput(sout, serr) 
+  proc.waitForOrKill(9000000)
+  println sout
+  println serr
+  tfvars.delete()
+}
+
 // Treat here and down as main()
 // Jenkins pipeline would pass around vars in / out instead of this file 
 println "Starting..."
 def String blue = get_blue(terragrunt_working_dir)
 println "Blue infrastrucure is ${blue}"
+change_attach_asg_to(blue, terragrunt_working_dir)
+
+
+
+
+//String[] ENVtoArray() { ENV.collect { k, v -> "$k=$v" } }
+//
+//"bash -c set".execute(ENVtoArray(), null).text 
+
+

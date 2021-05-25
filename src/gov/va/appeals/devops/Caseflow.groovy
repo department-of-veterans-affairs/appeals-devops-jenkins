f0445e9bfc6ecefe6d751b1a3543613c424163d2
Joel Robinson <jrobinson@tistatech.com>
2021-05-25 17:56:27 -0500

    Added DEPLOY_TYPE to the Caseflow class.

--- src/gov/va/appeals/devops/Caseflow.groovy
+++ src/gov/va/appeals/devops/Caseflow.groovy
@@ -3,4 +3,9 @@ package gov.va.appeals.devops
 class Caseflow {
 static final CASEFLOW_APPS =  ['certification', 'efolder', 'monitor']
 static final CASEFLOW_DB_APPS =  ['certification', 'efolder']
+static final DEPLOY_TYPE = [
+                           certification: 'deploys',
+                           efolder: 'deploys',
+                           monitor: 'blueGreens'
+                           ]
 }

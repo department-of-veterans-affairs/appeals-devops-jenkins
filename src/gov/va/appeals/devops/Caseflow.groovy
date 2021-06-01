a225946bb16f819c3f25fc452cc6b607581f80bb
Joel Robinson <jrobinson@tistatech.com>
2021-06-01 09:55:13 -0500

    Update constant name.

--- src/gov/va/appeals/devops/Caseflow.groovy
+++ src/gov/va/appeals/devops/Caseflow.groovy
@@ -2,7 +2,7 @@ package gov.va.appeals.devops
 
 class Caseflow {
 static final CASEFLOW_APPS =  ['certification', 'efolder', 'monitor']
-static final CASEFLOW_DB_APPS =  ['certification', 'efolder']
+static final CASEFLOW_APPS_WITH_DB =  ['certification', 'efolder']
 static final DEPLOY_TYPE = [
                            certification: 'deploys',
                            efolder: 'deploys',

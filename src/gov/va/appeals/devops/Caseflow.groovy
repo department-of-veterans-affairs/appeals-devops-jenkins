42e1500ae868ca22e1890ca3090a55e629ebf345
Anya Roltsch <aroltsch@tistatech.com>
2021-06-21 12:44:08 -0400

    Add sentry

--- src/gov/va/appeals/devops/Caseflow.groovy
+++ src/gov/va/appeals/devops/Caseflow.groovy
@@ -3,9 +3,11 @@ package gov.va.appeals.devops
 class Caseflow {
 static final CASEFLOW_APPS =  ['certification', 'efolder', 'monitor']
 static final CASEFLOW_APPS_WITH_DB =  ['certification', 'efolder']
+static final UTILITY_APPS =  ['sentry']
 static final DEPLOY_TYPE = [
                            certification: 'deploys',
                            efolder: 'deploys',
-                           monitor: 'blueGreens'
+                           monitor: 'blueGreens',
+                           sentry: "blueGreens"
                            ]
 }

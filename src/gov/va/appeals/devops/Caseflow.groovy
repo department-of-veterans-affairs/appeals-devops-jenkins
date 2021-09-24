a39d99c5c97d1909dc9a69f14ddf3e985a43ed06
Joel Robinson <jrobinson@tistatech.com>
2021-09-24 11:32:54 -0500

    Move efolder to blue-green deployment.

--- src/gov/va/appeals/devops/Caseflow.groovy
+++ src/gov/va/appeals/devops/Caseflow.groovy
@@ -6,9 +6,9 @@ static final CASEFLOW_APPS_WITH_DB =  ['certification', 'efolder']
 static final UTILITY_APPS =  ['sentry']
 static final DEPLOY_TYPE = [
                            certification: 'deploys',
-                           efolder: 'deploys',
+                           efolder: 'blueGreens',
                            monitor: 'blueGreens',
-                           sentry: "blueGreens"
+                           sentry: 'blueGreens'
                            ]
 static final SCALE_DOWN = [
                             certification: [

7fc898f3a201516700ad0c650a5e2f8a0a0c46cf
Joel Robinson <jrobinson@tistatech.com>
2021-12-23 07:53:17 -0600

    Change application name in SCALE_DOWN

--- src/gov/va/appeals/devops/Caseflow.groovy
+++ src/gov/va/appeals/devops/Caseflow.groovy
@@ -8,6 +8,7 @@ class Caseflow {
   static final CASEFLOW_APPS =  ['certification', 'efolder', 'monitor']
   static final CASEFLOW_APPS_WITH_DB =  ['certification', 'efolder']
   static final UTILITY_APPS =  ['sentry', 'logstash']
+  // Application Name in Jenkins
   static final DEPLOY_TYPE = [
                               certification: 'blueGreens',
                               efolder: 'blueGreens',
@@ -15,8 +16,9 @@ class Caseflow {
                               sentry: 'blueGreens',
                               logstash: 'blueGreens'
                            ]
+  // Application Name in Terragrunt
   static final SCALE_DOWN = [
-                            certification: [
+                            caseflow: [
                                            maxSize: 3,
                                            minSize: 1,
                                            desiredCapacity: 2

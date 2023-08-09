2b09925633106a18fd9d5232584a681e704f8c22
Dwayne Davis <davis_dwayne@bah.com>
2023-08-09 14:01:16 -0400

    add dbread to library

--- src/gov/va/appeals/devops/Caseflow.groovy
+++ src/gov/va/appeals/devops/Caseflow.groovy
@@ -7,13 +7,14 @@ class Caseflow {
 
   static final CASEFLOW_APPS =  ['certification', 'efolder', 'monitor']
   static final CASEFLOW_APPS_WITH_DB =  ['certification', 'efolder']
-  static final UTILITY_APPS =  ['sentry', 'logstash']
+  static final UTILITY_APPS =  ['sentry', 'logstash', 'caseflow-db-read']
   static final DEPLOY_TYPE = [
                               certification: 'blueGreens',
                               efolder: 'blueGreens',
                               monitor: 'blueGreens',
                               sentry: 'blueGreens',
-                              logstash: 'blueGreens'
+                              logstash: 'blueGreens',
+                              caseflow-db-read: 'blueGreens'
                            ]
   static final SCALE_DOWN = [
                             certification: [

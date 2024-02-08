c31c1e9df46fd49d7994f9e40d23830fc84893b6
Dwayne Davis <davis_dwayne@bah.com>
2024-02-08 16:17:33 -0500

    kafka to utility app so not kick off post depoyment

--- src/gov/va/appeals/devops/Caseflow.groovy
+++ src/gov/va/appeals/devops/Caseflow.groovy
@@ -7,13 +7,14 @@ class Caseflow {
 
   static final CASEFLOW_APPS =  ['certification', 'efolder', 'monitor']
   static final CASEFLOW_APPS_WITH_DB =  ['certification', 'efolder']
-  static final UTILITY_APPS =  ['sentry', 'logstash', 'caseflow-db-read']
+  static final UTILITY_APPS =  ['sentry', 'logstash', 'caseflow-db-read', 'kafka']
   static final DEPLOY_TYPE = [
                               certification: 'blueGreens',
                               efolder: 'blueGreens',
                               monitor: 'blueGreens',
                               sentry: 'blueGreens',
                               logstash: 'blueGreens',
+                              kafka: 'blueGreens',
                               "caseflow-db-read": 'blueGreens'
                            ]
   static final SCALE_DOWN = [
